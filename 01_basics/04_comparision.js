0ccdb1d0a2c0bb9fe55f47bf4fd8d52de3d5eb6e
Ritesh-Parmar <riteshparmar3612@gmail.com>
2025-01-09 13:21:42 +0000

    datatypes summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+//doing irrelevant comparisons is a bad practice... try avoiding it
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// the "==" operator does not convert null to anything ... but comparision opertators convert null to 0
+// the"===" operator helps equating a value along with its datatype.. so no conversion is needed here.. 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
